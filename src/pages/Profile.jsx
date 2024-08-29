b5a1013dc0ceb99f682655b45bcbc1d51373264c
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-08-29 11:07:31 +0200

    Some new changes in styled-profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Button from '../components/button/Button.jsx';
-import Person from '../assets/images/Profile.png';
-import {Greeting, Logo, Title, Name, MyProfile} from '../styled/styled-profile.jsx';
+import ProfileImg from '../assets/images/Profile.png';
+import {Body, Greeting, Logo, Title, Name, MyProfile} from '../styled/styled-profile.jsx';
 
 
 
 const Profile = () => {
     return (
         <>
-
+            <Body>
             <Greeting><h1>Hi, Lara</h1></Greeting>
             <Logo></Logo>
             <Title>Profile</Title>
-            <Person></Person>
+            <MyProfile src={ProfileImg} alt="Profile"></MyProfile>
             <Name>Lara</Name>
             <Button nameButton="Personal Information"/>
             <Button nameButton="My Itineraries"/>
             <Button nameButton="Settings"/>
             <Button nameButton="Log Out"/>
             <Button nameButton="Search Itineraries"/>
-
+</Body>
         </>
     )
 }
--- src/styled/styled-profile.jsx
+++ src/styled/styled-profile.jsx
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
-
 export const Title = styled.p`
-font-size: 16PX;
+font-size: 20px;
 margin-bottom: 40px;
 font-family: 'Inter', sans-serif;
+color: #33415C
 `;
+
 
 export const Body = styled.div`
 display: flex;
 flex-direction: column;
-justify-content: space-between;
 align-items: center;
 height: 100vh;
 text-align: center;
@@ -19,6 +19,8 @@ color: white;
 padding: 20px;
 box-sizing: border-box;
 margin:0;
+position: relative;
+background-size:cover;
 
 @media (max-width: 768px) {
 }
@@ -28,8 +30,10 @@ export const Logo = styled.img`
 width: 80px;
 height: 80px;
 margin-top: 5px;
-margin.left: 90px;
+margin-right: 90px;
 ` ;
+
+
 
 export const MyProfile = styled.img`
 width: 80px;
@@ -44,8 +48,13 @@ font-family: 'Inter', sans-serif;
 ` ;
 
 export const Greeting =  styled.h1`
-font-size: 18PX;
+font-size: 16px;
 color: white;
 margin-bottom: 40px;
 font-family: 'Inter', sans-serif;
+position: absolute;
+top: 20px;
+left:20px;
+margin: 0;
+
 ` ;
