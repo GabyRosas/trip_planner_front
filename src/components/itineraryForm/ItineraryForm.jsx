ca61af7f3040bdee1e62fca37734cf9e8d6f3447
Fire-Fairy84 <esther.sarasua@gmail.com>
2024-08-31 11:32:18 +0200

    merge to dev

--- src/components/itineraryForm/ItineraryForm.jsx
+++ src/components/itineraryForm/ItineraryForm.jsx
@@ -1,53 +1,66 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { StyledForm, StyledInput, StyledSelect, StyledButton, Label } from './formStyled';
+import React from "react";
+import { useForm } from "react-hook-form";
+import {
+  StyledForm,
+  StyledInput,
+  StyledSelect,
+  StyledButton,
+  Label,
+} from "./formStyled";
 
 const ItineraryForm = () => {
-    const { register, handleSubmit, setError } = useForm();
+  const { register, handleSubmit, setError } = useForm();
 
-    const validCities = ['Barcelona', 'Ibiza', 'Santiago de Compostela', 'Sevilla', 'Oviedo'];
+  const validCities = [
+    "Barcelona",
+    "Ibiza",
+    "Santiago de Compostela",
+    "Sevilla",
+    "Oviedo",
+  ];
 
-    const validateCity = (value) => {
-        if (!validCities.includes(value)) {
-            window.alert('This city is not available in our database');
-            return false;
-        }
-        return true;
-    };
+  const validateCity = (value) => {
+    if (!validCities.includes(value)) {
+      window.alert("This city is not available in our database");
+      return false;
+    }
+    return true;
+  };
 
-    const onSubmit = handleSubmit((values) => {
-        if (validateCity(values.City)) {
-            alert('Form submit: ' + JSON.stringify(values));
-        } else {
-            setError('City', { type: 'validate', message: 'Invalid city' });
-        }
-    });
+  const onSubmit = handleSubmit((values) => {
+    if (validateCity(values.City)) {
+      alert("Form submit: " + JSON.stringify(values));
+    } else {
+      setError("City", { type: "validate", message: "Invalid city" });
+    }
+  });
 
-    return (
-        <StyledForm onSubmit={onSubmit}>
-            <Label htmlFor="city">City</Label>
-            <StyledInput
-                {...register('City', {
-                    required: 'City is mandatory',
-                })}
-                placeholder='Barcelona'
-            />
+  return (
+    <StyledForm onSubmit={onSubmit}>
+      <Label htmlFor="city">City</Label>
+      <StyledInput
+        {...register("City", {
+          required: "City is mandatory",
+        })}
+        placeholder="Barcelona"
+      />
 
-            <Label htmlFor="duration">Duration</Label>
-            <StyledSelect
-                {...register('Duration', { required: 'Duration is mandatory' })}
-                defaultValue=""
-            >
-                <option value="" disabled>Select number of days</option>
-                <option value="1 day">1 day</option>
-                <option value="2 days">2 days</option>
-                <option value="3 days">3 days</option>
-            </StyledSelect>
+      <Label htmlFor="duration">Duration</Label>
+      <StyledSelect
+        {...register("Duration", { required: "Duration is mandatory" })}
+        defaultValue=""
+      >
+        <option value="" disabled>
+          Select number of days
+        </option>
+        <option value="1 day">1 day</option>
+        <option value="2 days">2 days</option>
+        <option value="3 days">3 days</option>
+      </StyledSelect>
 
-            <StyledButton type='submit'>Generate itinerary</StyledButton>
-        </StyledForm>
-    );
+      <StyledButton type="submit">Generate itinerary</StyledButton>
+    </StyledForm>
+  );
 };
 
 export default ItineraryForm;
-
